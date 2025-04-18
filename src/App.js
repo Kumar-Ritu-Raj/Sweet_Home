68eeaea3f40b89a65cbcf7b62d3304e2f6b111a5
KumarRituRaj <kumarrituraj2000@gmail.com>
2025-04-18 16:50:41 +0530

    fix: try1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import AddApartmentForm from "./components/AddApartmentForm.tsx";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddApartmentForm from "./components/AddApartmentForm.tsx";
 import ApartmentDetails from "./components/ApartmentDetails.tsx";
 import HomePage from "./components/Home.tsx";
 import SignInSignUp from "./components/SignInSignUp.tsx";
@@ -8,14 +8,14 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename="/Sweet_Home">
       <Routes>
-        <Route path="/" element={< HomePage/>} />
-        <Route path="/login" element={< SignInSignUp/>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<SignInSignUp />} />
         <Route path="/apartment-form" element={<AddApartmentForm />} />
         <Route path="/apartment-details" element={<ApartmentDetails />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
